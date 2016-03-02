6e0ca56ed946fd240017f50050d714b3529da81b
Gendai <t.brenet@orange.fr>
2016-03-02 11:41:00 +0000

    Milestones01

--- app/src/main/java/com/example/english/todo_list/MainActivity.java
+++ app/src/main/java/com/example/english/todo_list/MainActivity.java
@@ -4,13 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+    Button addb;
+    EditText edt;
+    ListView lstv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addb = (Button) findViewById(R.id.buttonADD);
+        edt = (EditText) findViewById(R.id.textbox);
+        lstv = (ListView) findViewById(R.id.ListView);
     }
 
     @Override
